0c46631928739b10fb958024cb9ee81f092e5de5
FurahaJustine <uwizefuraha@gmail.com>
2025-02-11 20:27:24 +0200

    convert from decimal to hexadecimal

---
+++ dec_to_hexa.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    
+   
+    printf("Enter a decimal number: ");
+    scanf("%d", &num);
+    
+    
+    printf("Hexadecimal equivalent: %X\n", num);
+    
+    return 0;
+}
