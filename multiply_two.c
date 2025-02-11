4f6a3d3094d25f839470084139984fcd34ceaae7
FurahaJustine <uwizefuraha@gmail.com>
2025-02-11 19:58:14 +0200

    removing add_ten

---
+++ multiply_two.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num, result;
+    
+    
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    
+    result = num * 2;
+    
+   
+    printf("Result after multiplying by 2: %d\n", result);
+    
+    return 0;
+
+}
